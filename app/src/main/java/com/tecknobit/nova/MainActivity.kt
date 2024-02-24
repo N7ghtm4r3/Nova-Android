629ef4e3fea41bf7b3c5098ecb72608c5adc7110
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2024-02-24 18:07:38 +0100

    Starting works on MainActivity

--- app/src/main/java/com/tecknobit/nova/MainActivity.kt
+++ app/src/main/java/com/tecknobit/nova/MainActivity.kt
@@ -4,11 +4,27 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.tecknobit.nova.ui.theme.NovaTheme
 import com.tecknobit.nova.ui.theme.md_theme_light_primary
 
@@ -21,18 +37,51 @@ class MainActivity : ComponentActivity() {
         setContent {
             NovaTheme {
                 Scaffold (
-                    topBar = {
-                        LargeTopAppBar(
-                            title = {
-                                Text("gagag")
-                            },
-                            colors = TopAppBarDefaults.largeTopAppBarColors(
-                                containerColor = md_theme_light_primary
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = {  },
+                            containerColor = md_theme_light_primary
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Add,
+                                contentDescription = null
                             )
-                        )
+                        }
                     }
                 ) {
-
+                    Column (
+                        modifier = Modifier
+                            .background(md_theme_light_primary)
+                            .fillMaxSize(),
+                    ) {
+                        Spacer(modifier = Modifier.height(125.dp))
+                        Card (
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight(),
+                            shape = RoundedCornerShape(
+                                topStart = 35.dp,
+                                topEnd = 35.dp
+                            ),
+                            elevation = CardDefaults.cardElevation(15.dp),
+                        ) {
+                            Column (
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(
+                                        top = 15.dp,
+                                        end = 15.dp,
+                                        bottom = 15.dp,
+                                        start = 20.dp
+                                    )
+                            ) {
+                                Text(
+                                    text = getString(R.string.projects),
+                                    fontSize = 22.sp
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
